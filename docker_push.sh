018b60f48f646490adb7d3717afca09b075251df
Aniket Chopade <aniketchopade@gmail.com>
2019-08-20 12:48:52 +0530

    update deploy scripts

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/site
+pip install --upgrade pip
 pip install --upgrade --user awscli
 export PATH=~/.local/bin:$PATH
-aws --version
+aws ecs update-service --cluster my-cluster --service aniketswebsite --force-new-deployment

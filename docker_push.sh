75b0bd94c26cf92fcc6d69b5f67dd1198c60d672
Aniket Chopade <aniketchopade@gmail.com>
2019-08-20 12:39:25 +0530

    docker push

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/site
+pip3 install --upgrade --user awscli
+export PATH=~/.local/bin:$PATH
+aws --version
